4c737ac278a7ffdfaf69e01458bb8800d560be7a
Raul Olaje <rolaje@moddtech.com>
2019-03-30 12:44:00 -0700

    cambio test

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -17,6 +17,6 @@ echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
 echo 'runner Jest (https://facebook.github.io/jest/).'
 set -x
-npm test
-npm audit | tail -n 2
+sudo npm test
+sudo npm audit | tail -n 2
 
